1a5d329948e6bafd3765503a56f31ddfa29a8fea
ocean <jojoceanus@outlook.com>
2025-12-22 12:29:37 +0800

    add c/cpp configs

---
+++ lisp/init-cc.el
@@ -0,0 +1,11 @@
+;;; init-cc.el --- Modern C config with use-package
+
+(use-package cc-mode
+  :ensure nil
+  :config
+  (setq c-default-style "bsd"
+        c-basic-offset 4
+        tab-width 4
+        indent-tabs-mode nil))  
+
+(provide 'init-cc)
